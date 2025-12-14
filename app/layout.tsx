e8478904696316dfe7eb9234b48dac22f07cc8b3
Rebilaz <zrebila@gmail.com>
2025-12-14 16:39:20 +0100

    1

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,7 +10,17 @@ export const metadata: Metadata = {
   title: "Systèmes & Dashboards",
   description:
     "Je connecte tes outils, j’automatise tes process et je crée des dashboards pour piloter ton entreprise simplement.",
+  icons: {
+    icon: [
+      { url: "/favicon.ico" }, // ⭐ Google
+      { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/favicon.svg", type: "image/svg+xml" },
+    ],
+    apple: "/apple-touch-icon.png",
+  },
 };
+
 
 export default function RootLayout({
   children,
